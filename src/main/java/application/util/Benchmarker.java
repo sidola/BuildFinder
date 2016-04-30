9ba34e3487765927fc1227169e1c4f8f63d3a1b1
sidola <mjauwe@gmail.com>
2016-04-30 11:44:16 +0200

    Adds a simple util class for benchmarking code

---
+++ src/main/java/application/util/Benchmarker.java
@@ -0,0 +1,52 @@
+package application.util;
+
+/**
+ * Simple utility class to get benchmarks. Does not work with nested calls.
+ * 
+ * @author Sid Botvin
+ */
+public final class Benchmarker {
+
+    // -----------------------------------
+    // Variables
+    // -----------------------------------
+
+    private static long startTime;
+
+    // -----------------------------------
+    // Constructor
+    // -----------------------------------
+
+    private Benchmarker() {
+    }
+
+    // -----------------------------------
+    // Methods
+    // -----------------------------------
+
+    /**
+     * Sets the start-time for the benchmark to the time this method was called.
+     */
+    public static void setStart() {
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Returns the elapsed duration (in milliseconds) between calling this
+     * method and when {@link Benchmarker#setStart()} was called.
+     * 
+     * @return The elapsed duration in milliseconds.
+     */
+    public static long getDuration() {
+
+        // Should never be 0 when this method is called
+        if (startTime == 0)
+            return -1;
+
+        long elapsedDuration = ((System.nanoTime() - startTime) / 1000000);
+        startTime = 0;
+
+        return elapsedDuration;
+    }
+
+}

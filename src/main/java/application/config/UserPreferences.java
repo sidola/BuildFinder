18faa863a2affe87c89991edfff2edfb54cda11b
sidola <mjauwe@gmail.com>
2016-05-01 11:18:34 +0200

    Adds new user preferences and fixes a bug with streams not being properly closed

--- src/main/java/application/config/UserPreferences.java
+++ src/main/java/application/config/UserPreferences.java
@@ -4,7 +4,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
+
+import org.jsoup.helper.StringUtil;
 
 public class UserPreferences {
 
@@ -16,6 +21,8 @@ public class UserPreferences {
 
     private static final Properties PREFERENCES = new Properties();
     private static final File PREFERENCES_FILE = new File("./data/", "user.preferences");
+
+    public final static String VALUE_SEPARATOR = ";";
 
     // ----------------------------------------------
     //
@@ -33,7 +40,9 @@ public class UserPreferences {
 
             } else {
 
-                PREFERENCES.load(new FileReader(PREFERENCES_FILE));
+                FileReader reader = new FileReader(PREFERENCES_FILE);
+                PREFERENCES.load(reader);
+                reader.close();
 
             }
         } catch (IOException e) {
@@ -59,6 +68,16 @@ public class UserPreferences {
         return Boolean.parseBoolean(PREFERENCES.getProperty(preferenceKey.key));
     }
 
+    public static List<String> getList(PrefKey preferenceKey) {
+        String stringList = PREFERENCES.getProperty(preferenceKey.key);
+
+        if (StringUtil.isBlank(stringList)) {
+            return Collections.emptyList();
+        }
+
+        return Arrays.asList(stringList.split(VALUE_SEPARATOR));
+    }
+
     public static <T> void set(PrefKey preferenceKey, T value) {
         PREFERENCES.setProperty(preferenceKey.key, value.toString());
         save();
@@ -66,18 +85,12 @@ public class UserPreferences {
 
     public static void save() {
         try {
-            PREFERENCES.store(new FileWriter(PREFERENCES_FILE), null);
+            FileWriter writer = new FileWriter(PREFERENCES_FILE);
+            PREFERENCES.store(writer, null);
+            writer.close();
         } catch (IOException e) {
             throw new RuntimeException("Failed to save user preferences", e);
         }
-    }
-
-    /**
-     * Debugging
-     */
-    @Deprecated
-    public static String peek() {
-        return PREFERENCES.toString();
     }
 
     // ----------------------------------------------
@@ -92,7 +105,10 @@ public class UserPreferences {
         
         BUILDS_URL("builds_url"),
         PAGE_COUNT("page_count"),
-        CHECK_FOR_UPDATES("check_for_updates");
+        CHECK_FOR_UPDATES("check_for_updates"),
+        
+        ADDITIONAL_BUILD_URLS("additional_build_urls"),
+        ADDITIONAL_PAGE_COUNTS("additional_page_counts");
         
         // @formatter:on
 
